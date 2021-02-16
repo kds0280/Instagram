98cfe93277c48c73f3aa415fd028178659114268
hay <kds0280@naver.com>
2021-02-16 19:22:09 +0900

    third commit

---
+++ src/post/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsMineOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user.id == obj.user.id
+        )
