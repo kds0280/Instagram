4411cdc06fb1d962d7f1433cc07bc2702665e285
hay <kds0280@naver.com>
2021-03-09 13:46:57 +0900

    finished PostLike API

--- src/instagram/urls.py
+++ src/instagram/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from post.views import PostListCreate, PostDetailUpdateDelete, CommentCreate, CommentDelete
+from post.views import PostListCreate, PostDetailUpdateDelete, CommentCreate, CommentDelete, PostLike
 from user.views import Login, SignUp, UserUpdate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post', PostListCreate.as_view()),
     path('post/<int:post_id>', PostDetailUpdateDelete.as_view()),
+    path('post/<int:post_id>/like', PostLike.as_view()),
     path('auth', Login.as_view()),
     path('signup', SignUp.as_view()),
     path('user/<int:user_id>', UserUpdate.as_view()),
--- src/post/permissions.py
+++ src/post/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsPostMineOrReadOnly(BasePermission):
+class IsObjectMineOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(
             request.method in SAFE_METHODS or
