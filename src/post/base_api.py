6849647ec2d2b2902683626e5bbe5935eaace749
hay <kds0280@naver.com>
2021-03-09 13:51:02 +0900

    Revert "modify CreatewithoutSerializer(modularity)"
    This reverts commit 127ad7c3111b8b9fa5c0658bd4cade811d92fe4c.

--- src/post/base_api.py
+++ src/post/base_api.py
@@ -11,18 +11,15 @@ class CreateAPIViewWithoutSerializer(generics.CreateAPIView):
     class_to_create_object = None
 
     def create(self, request, *args, **kwargs):
+        schema = self.schema
+        validator = MyValidator(schema)
         data = request.data.dict()
-        isvalid_data = self.check_validation(**data)
-        instance = self.create_instance(request, **isvalid_data)
+        data = self.check_validation(validator, **data)
+        instance = self.class_to_create_object.objects.create(**data, user=request.user)
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
 
-    def check_validation(self, **data):
-        schema = self.schema
-        validator = MyValidator(schema)
+    def check_validation(self, validator, **data):
         if not validator.validate(data):
             raise ValidationError
         return data
-
-    def create_instance(self, request, **isvalid_data):
-        return self.class_to_create_object.objects.create(**isvalid_data)
